44e374c03a395919d054d6f9740bd6f0d0120e28
Aladdin95 <alaa2005721@gmail.com>
2018-12-17 22:05:57 +0200

    adding tree of nodes represented as a list
    Co-authored-by: Abdullah Aly El-Sayed <abdullah.ali26@hotmail.com>

--- functions.py
+++ functions.py
@@ -1,3 +1,16 @@
+import inspect
+#inspect.getouterframes(inspect.currentframe(), 2)[1][3]
+
+class Node:
+    def __init__(self, parent, level, text):
+        self.parent = parent
+        self.level = level
+        self.text = text
+    
+    def __str__(self):
+        return self.text + f' | parent: {self.parent}'
+    
+
 def match(token):
     global i
     global tokens
@@ -11,128 +24,161 @@ def match(token):
 
 
 def program():
-    stmt_sequence()
+    stmt_sequence(-1)
     print('compiled successfully')
     
     
-def stmt_sequence():
+def stmt_sequence(parent):
     global i
     global tokens
     global level
     level += 1
-    statement()
+    statement(parent)
     while i < len(tokens) and tokens[i][1] == ';':
         match(';')
-        statement()
+        parent+=1
+        statement(parent)
     level -= 1
 
         
-def statement():
+def statement(parent):
     global i
     global tokens
 
-    if tokens[i][1] != 'identifier':
-        print(tokens[i][1], ' level:', level)
-    else:
-        print('assign', ' level:', level)
+    #if tokens[i][1] != 'identifier':
+    #    print(tokens[i][1], ' level:', level)
+    #else:
+    #    print('assign', ' level:', level)
 
     if tokens[i][1] == 'if':
-        if_stmt()
+        if_stmt(parent)
     elif tokens[i][1] == 'repeat':
-        repeat_stmt()
+        repeat_stmt(parent)
     elif tokens[i][1] == 'identifier':
-        assign_stmt()
+        assign_stmt(parent)
     elif tokens[i][1] == 'read':
-        read_stmt()
+        read_stmt(parent)
     elif tokens[i][1] == 'write':
-        write_stmt()
+        write_stmt(parent)
     else:
         raise ValueError('token mismatch')
     
 
-def if_stmt():
+def if_stmt(parent):
+    global level
     global i
     global tokens
     match('if')
     exp()
     match('then')
-    stmt_sequence()
+    stmt_sequence(parent)
     if tokens[i][1] == 'else':
         match('else')
-        stmt_sequence()
+        stmt_sequence(parent)
     match('end')
   
-
-def repeat_stmt():
+    
+def repeat_stmt(parent):
+    global level
+    global i
+    global tokens
     match('repeat')
-    stmt_sequence()
+    stmt_sequence(parent)
     match('until')
     exp()
     
     
-def assign_stmt():
+def assign_stmt(parent):
+    global level
+    global i
+    global tokens
     match('identifier')
     match(':=')
     exp()
     
     
-def read_stmt():
+def read_stmt(parent):
+    global level
+    global i
+    global tokens
     match('read')
+    nodes.append(Node(parent, level, f'read ({tokens[i][0]})'))
     match('identifier')
 
     
-def write_stmt():
-    match('write')
-    exp()
-    
-    
-def exp():
+def write_stmt(parent):
+    global level
     global i
     global tokens
-    simple_exp()
+    match('write')
+    nodes.append(Node(parent, level, 'write'))
+    exp(len(nodes)-1)
+    
+    
+def exp(parent):
+    global i
+    global tokens
+    x = simple_exp(parent)
     if tokens[i][1] == '<' or tokens[i][1] == '=':
+        nodes.append(Node(parent, level, f'op ({tokens[i][0]})'))
+        nodes[x].parent = len(nodes)-1
+        x = len(nodes)-1
         comparison_op()
-        simple_exp()
+        simple_exp(x)
 
         
 def comparison_op():
     match(tokens[i][1])
     
 
-def simple_exp():
+def simple_exp(parent):
     global i
     global tokens
-    term()
+    x = term(parent)
     while tokens[i][1] == '+' or tokens[i][1] == '-':
+        nodes.append(Node(parent, level, f'op ({tokens[i][0]})'))
+        nodes[x].parent = len(nodes)-1
+        x = len(nodes)-1
         addop()
-        term()
+        term(x)
+    return x
 
         
 def addop():
     match(tokens[i][1])
     
 
-def term():
+def term(parent):
     global i
     global tokens
-    factor()
+    x = factor(parent)
     while tokens[i][1] == '*' or tokens[i][1] == '/':
+        nodes.append(Node(parent, level, f'op ({tokens[i][0]})'))
+        nodes[x].parent = len(nodes)-1
+        x = len(nodes)-1
         mulop()
-        factor()
+        factor(x)
+    return x
         
         
 def mulop():
     match(tokens[i][1])
     
 
-def factor():
+def factor(parent):
     global i
     global tokens
     if tokens[i][1] == '(':
         match('(')
-        exp()
+        x = exp(parent)
         match(')')
+        return x
     elif tokens[i][1] == 'number':
+        nodes.append(Node(parent, level, f'const ({tokens[i][0]})'))
         match('number')
+
+
     elif tokens[i][1] == 'identifier':
+        nodes.append(Node(parent, level, f'id ({tokens[i][0]})'))
         match('identifier')
+    return len(nodes)-1
